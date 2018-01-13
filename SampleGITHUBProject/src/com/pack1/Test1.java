4752015217f5572e490058d395da740bfdcd5c47
pritam <pritam@DESKTOP-3PR64H6>
2018-01-13 13:25:11 +0530

    Add the number

--- SampleGITHUBProject/src/com/pack1/Test1.java
+++ SampleGITHUBProject/src/com/pack1/Test1.java
@@ -4,11 +4,13 @@ public class Test1 {
 
 	public static void main(String[] args) {
 
+		int sum =0;
 		System.out.println("Main Start");
 		for(int i=1; i<=10; i++)
 		{
-			System.out.println(i);
+			sum = sum+i;
 		}
+		System.out.println(sum);
 		System.out.println("Main End");
 		
 	}
