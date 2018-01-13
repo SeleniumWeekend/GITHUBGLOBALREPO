54008caea0c1f5636b48971a81d5b4b33de227e1
pritam <pritam@DESKTOP-3PR64H6>
2018-01-13 13:54:58 +0530

    mvbvmbvmbvmv

--- SampleGITHUBProject/src/com/pack1/Test1.java
+++ SampleGITHUBProject/src/com/pack1/Test1.java
@@ -3,7 +3,13 @@ package com.pack1;
 public class Test1 {
 
 	public static void main(String[] args) {
-
+		System.out.println(",fdnf,nf,fn");
+		System.out.println(",fdnf,nf,fn");
+		System.out.println(",fdnf,nf,fn");
+		System.out.println(",fdnf,nf,fn");
+		System.out.println(",fdnf,nf,fn");
+		System.out.println(",fdnf,nf,fn");
+		System.out.println(",fdnf,nf,fn");
 		int sum =0;
 		for(int i=1; i<=10; i++)
 		{
