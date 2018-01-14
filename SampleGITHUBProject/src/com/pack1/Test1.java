ff73fcf1a5522be7f9e623897ddd7b1887aff640
SHALINI KUMARI <SHALINI KUMARI@SHALINI>
2018-01-13 14:28:55 -1100

    Delete sop and added new one

--- SampleGITHUBProject/src/com/pack1/Test1.java
+++ SampleGITHUBProject/src/com/pack1/Test1.java
@@ -3,13 +3,8 @@ package com.pack1;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println(",fdnf,nf,fn");
-		System.out.println(",fdnf,nf,fn");
-		System.out.println(",fdnf,nf,fn");
-		System.out.println(",fdnf,nf,fn");
-		System.out.println(",fdnf,nf,fn");
-		System.out.println(",fdnf,nf,fn");
-		System.out.println(",fdnf,nf,fn");
+		System.out.println("Main Start");
+		System.out.println("Sum of n number");
 		int sum =0;
 		for(int i=1; i<=10; i++)
 		{
